9b341ecbdf5783bf428168e203dd75b246fee265
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-03 20:57:01 +0200

    Learned how to use the @Primary annotation

---
+++ src/main/java/es/fercbrt/springbootdi/repositories/impl/ProductRepositoryJPA.java
@@ -0,0 +1,25 @@
+package es.fercbrt.springbootdi.repositories.impl;
+
+import es.fercbrt.springbootdi.models.Product;
+import es.fercbrt.springbootdi.repositories.ProductRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
+
+@Primary
+@Repository
+public class ProductRepositoryJPA implements ProductRepository {
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(
+                new Product(1L, "Product from JPA", 100L)
+        );
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return new Product(id, "Product from JPA", 100L);
+    }
+}
