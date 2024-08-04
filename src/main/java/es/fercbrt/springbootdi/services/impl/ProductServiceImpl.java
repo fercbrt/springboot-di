f1ff357c23cb5ed18b17512f5405a8973fb257f1
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-04 13:33:31 +0200

    Self task done

--- src/main/java/es/fercbrt/springbootdi/services/impl/ProductServiceImpl.java
+++ src/main/java/es/fercbrt/springbootdi/services/impl/ProductServiceImpl.java
@@ -3,7 +3,9 @@ package es.fercbrt.springbootdi.services.impl;
 import es.fercbrt.springbootdi.models.Product;
 import es.fercbrt.springbootdi.repositories.ProductRepository;
 import es.fercbrt.springbootdi.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +13,8 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-
+    @Autowired
+    private Environment environment;
     private ProductRepository productRepository;
 
     public ProductServiceImpl(@Qualifier("productList") ProductRepository productRepository) {
@@ -21,7 +24,7 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> findAll() {
         return productRepository.findAll().stream().map(p -> {
                     Product product = p.clone();
-                    product.setPrice(product.getPrice() * 2);
+                    product.setPrice(product.getPrice() * environment.getProperty("config.price", Long.class));
                     return product;
 //                    p.setPrice(p.getPrice() * 2);
 //                    return p;
